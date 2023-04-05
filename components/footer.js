b06f9f6faad8bc04aef1026c57003f4284d33ef4
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-04-05 20:19:57 +0530

    add footer

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,12 @@
+import Image from "next/image";
+import buneo from "../public/Humankind_Assets/images/bluneo-logo.svg";
 const Footer = () => {
   return (
     <footer>
-      <div>Footer</div>
+      <div className="flex justify-center items-center flex-row my-6">
+        Powered by
+        <Image src={buneo} />
+      </div>
     </footer>
   );
 };
